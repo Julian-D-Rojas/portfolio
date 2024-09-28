782fd75e32ab70df659ddc086601c807c766d599
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2024-09-27 20:35:30 -0500

    fonts

--- src/assets/components/NavMenu.jsx
+++ src/assets/components/NavMenu.jsx
@@ -1,28 +1,38 @@
 import './NavMenu.css'
-import { PersonIcon } from './icons/PersonIcon'
 import { ProyectIcon } from './icons/ProyectIcon'
 import { ContactIcon } from './icons/ContactIcon'
+import { GithubIcon } from './icons/GithubIcon'
+import{LinkedinIcon} from './icons/LinkedinIcon'
+
 export const NavMenu = () => {
   let proyects = 'Proyectos'
   let contact = 'Contacto'
   return (
     <>
       <nav className="Nav">
+        <div className="Nav-div--container">
+
+        <div className="Nav-div--h1">
+            <a href="#top" className="Nav-a">
+      <h1 className='h1'>Julian Rojas</h1>
+            </a>
+
+        </div>
         <ul className="Nav-ul">
-          <li className="Nav-li"><div className="Nav-div-icon">
-            </div><a href="#top" className="Nav-a"><PersonIcon/><h1>Julian Rojas</h1>
-          
-            </a></li>
 
-          <li className="Nav-li"><div className="Nav-div-icon">
-          </div><a href="#proyects" className="Nav-a"><ProyectIcon/>{proyects}</a></li>
+          <li className="Nav-li">
+          <a href="#proyects" className="Nav-a"><div className="Nav-div-icon"><ProyectIcon/></div>{proyects}</a></li>
           
-          <li className="Nav-li"><div className="Nav-div-icon">
-
-            </div><a href="#contact" className="Nav-a"><ContactIcon/>{contact}</a></li>
+          <li className="Nav-li">
+          <a href="#proyects" className="Nav-a"><div className="Nav-div-icon"><ContactIcon/></div>{contact}</a></li>
         </ul>
+        </div>
+        <div className="Nav-div--social">
+          <LinkedinIcon/>
+          <GithubIcon />
+        </div>
       </nav>
-
+      
     </>
   )
 }
