f58066a976b1bb71c1e0eb07f4a9244397c5c96a
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2024-10-17 21:26:24 -0500

    Designer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,9 +10,10 @@ export const Contact = () => {
                         <input type="text" placeholder="Nombre" name="name"
                             title="Nombre sólo acepta letras, espacios en blanco y máximo 45 caracteres"
                             pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]{1,45}$" required />
+                        <input type="email" placeholder="Correo Electrónico" name="email" id="email" autoComplete="email" required></input>
                         <input type="number" placeholder="Numero Telefónico" name="id" pattern="^[0-9]{1,20}$" />
-                        <input type="mesagge" placeholder="Mensaje" />
-                        <button className="button-Contact"  type="submit" name="btn-submit">{(key) ? 'Enviar' : <div className="loader"></div>}</button> 
+                        <textarea id="message" placeholder="Mensaje.." required></textarea>
+                        <button className="button-Contact"  type="submit" name="btn-submit">{(key) ?  <div className="loader"></div>:'Enviar' }</button> 
                     </form>
 
                 </div>
--- src/components/Porfile.jsx
+++ src/components/Porfile.jsx
@@ -1,42 +1,62 @@
 import './Porfile.css'
 import julianImage from '../assets/img/Julian Rojas.jpg'
+import { ContactIcon } from './icons/ContactIcon'
+import {CvIcon} from './icons/CvIcon'
 export const Porfile = () => {
-  return (
-    <>
-    <div className="div-Porfile--container">
-        <h2 className="h2-Porfile">Julian Rojas</h2>
-        <figure className="figure-Porfile">
-            <img src={julianImage} alt="Julian Rojas" className="img-Porfile" />
-        </figure>
-        <h3 className="h3-Porfile">Ingeniero en Telecomunicaciones</h3>
-        <p className="p-Porfile">Ingeniero con conocimientos en diseño responsivo de
-páginas web con HTML, CSS, SASS , JavaScript y
-ReactDOM; API REST, java, manejo de base de datos SQL,
-GIT y GitHub, me caracterizo por:</p>
-<ul className="ul-Porfile">
-    <li className="li-Porfile">
-Creatividad, proactividad, análisis y perseverancia
-para cumplir objetivos.
+    return (
+        <>
+            <div className="div-Porfile--name">
+                <a href="#top"  className="Nav-a">
+                <h2 className="h2-Porfile">Julian Rojas</h2> </a>
+                </div>
+            <div className="div-Porfile--container">
+                <div className="div-Porfile--coverImage">
 
-    </li>
-    <li className="li-Porfile">
+                    <img src='https://i.pinimg.com/originals/8d/0b/88/8d0b883ff3b1abbf1dc7369f6b895443.gif
+' alt="Julian Rojas" className="img-Porfile--coverImage" />
+                </div>
+              
+                <figure className="figure-Porfile">
+                    <img src={julianImage} alt="Julian Rojas" className="img-Porfile" />
+                </figure>
 
-Trabajo en equipo.
-    </li>
-    <li className="li-Porfile">
+                <div className="div-Porfile--info">
+                <h3 className="h3-Porfile">Desarrollador <span className='greenColor'>Web</span> <span className="blueColor">Frontend</span> </h3>
+                <p className="p-Porfile">Ingeniero en Telecomunicaciones con conocimientos en diseño responsivo de
+                    páginas web con HTML, CSS, SASS , JavaScript y
+                    ReactDOM; API REST, java, manejo de base de datos SQL,
+                    GIT y GitHub, me caracterizo por:</p>
+                <ul className="ul-Porfile">
+                    <li className="li-Porfile">
+                        Creatividad, proactividad, análisis y perseverancia
+                        para cumplir objetivos.
 
-Facilidad para la escucha, el habla, la escritura y la
-lectura.
-    </li>
-    <li className="li-Porfile">
+                    </li>
+                    <li className="li-Porfile">
 
-Respeto, tolerancia y resiliencia ante las dificultades.
-    </li>
-</ul>
-<a href="#contact">
-        <button className="button-Porfile">Contacto</button>
-</a>
-    </div>
-    </>
-  )
+                        Trabajo en equipo.
+                    </li>
+                    <li className="li-Porfile">
+
+                        Facilidad para la escucha, el habla, la escritura y la
+                        lectura.
+                    </li>
+                    <li className="li-Porfile">
+
+                        Respeto, tolerancia y resiliencia ante las dificultades.
+                    </li>
+                </ul>
+<div className="div-Porfile-infoButton">
+                <a href="#contact">
+                    <button className="button-Porfile greenColorBackgorund"><CvIcon/></button>
+                </a>
+                <a href="#contact">
+                    <button className="button-Porfile blackColorBackgorund"><ContactIcon/></button>
+                </a>
+</div>
+                </div>
+                
+            </div>
+        </>
+    )
 }
