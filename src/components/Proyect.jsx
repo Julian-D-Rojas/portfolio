c004b509236b20f2b38bdc6691ad9576ba801e6b
Julian-D-Rojas <julian-d-rojas@hotmail.com>
2025-04-01 17:10:03 -0500

    update links

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -1,5 +1,5 @@
 import './Proyect.css'
-import {AirplaneIcon} from './icons/AirplaneIcon'
+import { AirplaneIcon } from './icons/AirplaneIcon'
 import { GithubIconBlack } from './icons/GithubIconBlack'
 import jardinBarquito from '../assets/img/Jardin Barquito.png'
 import { JavascriptIcon } from './icons/JavascriptIcon'
@@ -7,25 +7,50 @@ import { SassIcon } from './icons/SassIcon'
 export const Proyect = () => {
   return (
     <>
-   <div className="div-Proyect--container">
-    <div className="div-Proyect--imgContainer">
-        <img src={jardinBarquito} alt="" className="img-Proyect" />
-    </div>
-    <div className="div-Proyect--info">
-        <h3 className='h3-Proyect--info'>Jardín Infantil Barquito de Papel</h3>
-        <p className='p-Proyect'>El sitio web de Jardín Barquito de Papel está desarrollado con vanilla JavaScript y Sass para lograr una navegación rápida y funcionalidad fluida sin frameworks externos.</p>
-        <div className="div-Proyect--technologies">
-            <JavascriptIcon/>
-            <SassIcon/>
+      <div className='div-Proyect--container'>
+        <div className='div-Proyect--imgContainer'>
+          <img src={jardinBarquito} alt='' className='img-Proyect' />
         </div>
-        <div className="div-Proyect--buttons">
-          <a href="https://jardinbarquitodepapel.com/">
-            <button className="button-Proyect--first"><div className="div-center"><AirplaneIcon/> Ver Más</div></button></a>
-            <a href="https://github.com/Julian-D-Rojas/Barquito_de_Papel">
-            <button className="button-Proyect--second"><div className="div-center"><GithubIconBlack/>Código</div></button></a>
+        <div className='div-Proyect--info'>
+          <h3 className='h3-Proyect--info'>
+            Jardín Infantil Barquito de Papel
+          </h3>
+          <p className='p-Proyect'>
+            El sitio web de Jardín Barquito de Papel está desarrollado con
+            vanilla JavaScript y Sass para lograr una navegación rápida y
+            funcionalidad fluida sin frameworks externos.
+          </p>
+          <div className='div-Proyect--technologies'>
+            <JavascriptIcon />
+            <SassIcon />
+          </div>
+          <div className='div-Proyect--buttons'>
+            <a
+              href='https://julian-d-rojas.github.io/Barquito_de_Papel'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='button-Proyect--first'>
+                <div className='div-center'>
+                  <AirplaneIcon /> Ver Más
+                </div>
+              </button>
+            </a>
+            <a
+              href='https://github.com/Julian-D-Rojas/Barquito_de_Papel'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='button-Proyect--second'>
+                <div className='div-center'>
+                  <GithubIconBlack />
+                  Código
+                </div>
+              </button>
+            </a>
+          </div>
         </div>
-    </div>
-   </div>
+      </div>
     </>
   )
 }
--- src/components/Proyect2.jsx
+++ src/components/Proyect2.jsx
@@ -7,22 +7,47 @@ import { ReactIcon } from './icons/ReactIcon'
 export const Proyect2 = () => {
   return (
     <>
-      <div className="div-Proyect--container">
-        <div className="div-Proyect--imgContainer">
-          <img src={TDT} alt="" className="img-Proyect" />
+      <div className='div-Proyect--container'>
+        <div className='div-Proyect--imgContainer'>
+          <img src={TDT} alt='' className='img-Proyect' />
         </div>
-        <div className="div-Proyect--info">
-          <h3 className='h3-Proyect--info'>Administración de espacios en Blanco TDT</h3>
-          <p className='p-Proyect'>El sitio web de Administración de espacios en Blanco TDT está desarrollado con ReactJs y Tailwind para lograr una navegación rápida y funcionalidad fluida.</p>
-          <div className="div-Proyect--technologies">
+        <div className='div-Proyect--info'>
+          <h3 className='h3-Proyect--info'>
+            Administración de espacios en Blanco TDT
+          </h3>
+          <p className='p-Proyect'>
+            El sitio web de Administración de espacios en Blanco TDT está
+            desarrollado con ReactJs y Tailwind para lograr una navegación
+            rápida y funcionalidad fluida.
+          </p>
+          <div className='div-Proyect--technologies'>
             <ReactIcon />
             <TailwindIcon />
           </div>
-          <div className="div-Proyect--buttons">
-            <a href="https://github.com/Julian-D-Rojas/DTTManagerFrontend">
-              <button className="button-Proyect--first"><div className="div-center"><AirplaneIcon /> Ver Más</div></button></a>
-            <a href="https://github.com/Julian-D-Rojas/DTTManagerFrontend">
-              <button className="button-Proyect--second"><div className="div-center"><GithubIconBlack />Código</div></button></a>
+          <div className='div-Proyect--buttons'>
+            <a
+              href='https://github.com/Julian-D-Rojas/DTTManagerFrontend'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='button-Proyect--first'>
+                <div className='div-center'>
+                  <AirplaneIcon /> Ver Más
+                </div>
+              </button>
+            </a>
+            <a
+              href='https://github.com/Julian-D-Rojas/DTTManagerFrontend'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='button-Proyect--second'>
+                <div className='div-center'>
+                  <GithubIconBlack />
+                  Código
+                </div>
+              </button>
+            </a>
           </div>
         </div>
       </div>
